cb3bfe3595c42ebd91f8628f9c5faa284f85366b
linqing2022 <116869750+linqing2022@users.noreply.github.com>
2022-12-16 21:27:18 +0800

    ltsm.py

---
+++ 深度学习实战/LSTM/lstm.py
@@ -0,0 +1,6 @@
+import os
+os.system('python DataDownload.py')
+os.system('python feature_index.py')
+os.system('python features1.py')
+os.system('python features2.py')
+os.system('ipython NN.ipynb')
